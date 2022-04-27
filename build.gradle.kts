6121e188ec59bb58f56f1e3c1d6a0df683162a06
Wave1994-Hoon <kk03187693@gmail.com>
2022-04-27 23:24:26 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,11 @@ repositories {
     mavenCentral()
 }
 
+
+
+
+
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
